ee3f90b10cb8efb4d71fd107911c6a5782797b95
mintusmaximus <jheikkinen109@gmail.com>
2026-01-17 16:31:32 +0200

    more nav

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons, Feather } from "@expo/vector-icons";
 import HomeScreen from "../screens/HomeScreen";
 import NewScreen from "../screens/NewScreen";
 import ProfileScreen from "../screens/ProfileScreen";
@@ -10,16 +10,62 @@ const Tab = createBottomTabNavigator();
 
 const BottomNavigation = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Weather" component={WeatherScreen} />
-      <Tab.Screen name="New" component={NewScreen} />
-      <Tab.Screen name="Stats" component={StatsScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#1E1E1E",
+        tabBarInactiveTintColor: "#A9A9A9",
+        // maybe some more font styles here but im too lazy
+      }}
+      // change this if feels bad to use, history/fullHistory maybe
+      backBehavior="firstRoute"
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Weather"
+        component={WeatherScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="wind" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="New"
+        component={NewScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Stats"
+        component={StatsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="pulse" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
 
 export default BottomNavigation;
-
-const styles = StyleSheet.create({});

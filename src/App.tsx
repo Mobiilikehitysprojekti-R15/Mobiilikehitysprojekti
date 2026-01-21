8d873cf90253facf6c21f7fe663dab50dce23a3e
N1ppexd <niilohanni@gmail.com>
2026-01-21 03:07:46 +0200

    jees

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import BottomNavigation from "./components/BottomNavigation";
 import { NavigationContainer } from "@react-navigation/native";
-import  WeatherTest from "./screens/Weathertest";
 
 
 export default function App() {

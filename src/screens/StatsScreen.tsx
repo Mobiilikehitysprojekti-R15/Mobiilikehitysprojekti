1d1713a8977cc5d49a76109636293a431abde658
N1ppexd <niilohanni@gmail.com>
2026-01-23 02:09:46 +0200

    stat testailua

--- src/screens/StatsScreen.tsx
+++ src/screens/StatsScreen.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
+import StatisticsTest from "./StatisticsTest";
 type Props = {};
 
 const StatsScreen = (props: Props) => {
   return (
     <View>
       <Text>This is stats StatsScreen</Text>
+      <StatisticsTest />
     </View>
   );
 };

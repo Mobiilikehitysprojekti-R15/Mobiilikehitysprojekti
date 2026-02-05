4e0b63a1d1d57a8e42d80aff8a36d811ce1d6bfb
mintusmaximus <jheikkinen109@gmail.com>
2026-02-05 17:41:19 +0200

    Merge main

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import StyledButton from "../components/StyledButton";
 import ProfileAuth from "../components/ProfileAuth";
 import { useAuth } from "../context/AuthContext";
@@ -7,6 +7,9 @@ import { useTheme } from "../context/ThemeContext";
 import { db } from "../config/firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { UserProfile } from "../types/auth";
+import * as ImagePicker from "expo-image-picker";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { updateDoc } from "firebase/firestore";
 
 type Props = {};
 
@@ -58,18 +61,48 @@ const ProfileScreen = (props: Props) => {
         <Text style={{ color: theme.colors.textSecondary }}>Loading profile...</Text>
       ) : profile ? (
         <>
-          <Text style={{ color: theme.colors.text }}>Name: {profile.name}</Text>
-          <Text style={{ color: theme.colors.text }}>Email: {profile.email}</Text>
-          <Text style={{ color: theme.colors.text }}>License Type: {profile.licenseType}</Text>
-          <Text style={{ color: theme.colors.text }}>Phone Number: {profile.phoneNumber}</Text>
-          <Text style={{ color: theme.colors.text }}>Address: {profile.address}</Text>
-          <Text style={{ color: theme.colors.text }}>Date of Birth: {formatDate(profile.dateOfBirth)}</Text>
-          <Text style={{ color: theme.colors.text }}>Member Since: {formatDate(profile.createdAt)}</Text>
+          {/*PICTURE, NAME AND EMAIL centered, ei voi vielä asettaa profiilikuvaa*/}
+          <View style={styles.nameCard}>
+            <Image source={require("../assets/avatar-placeholder.png")}style={styles.avatar}/>
+            <View style={styles.nameText}>
+              <Text style={styles.bold}>{profile.name || "Your name"}</Text>
+              <Text>{profile.email || "Your email"}</Text>
+            </View>
+          </View>
+
+          {/*LICENSE*/}
+          <View style={styles.headingCard}>
+            <Text style={styles.bold}>License</Text>
+            <Text>{profile?.licenseType || "Student/A/B/C/D"}</Text>
+          </View>
+
+          {/*PERSONAL INFO*/}
+          <View style={styles.headingCard}>
+            <Text style={styles.bold}>Personal info</Text>
+            <Text>Phone Number: {profile.phoneNumber}</Text>
+            <Text>Address: {profile.address}</Text>
+            <Text>Date of Birth: {formatDate(profile.dateOfBirth)}</Text>
+            <Text>Member Since: {formatDate(profile.createdAt)}</Text>
+
+            {/*CHANGE INFO button, ei tapahu vielä mitään tästä*/}
+            <StyledButton title="Change Info" onPress={() => {}} />
+          </View>
+
+          <View style={styles.headingCard}>
+            <Text style={styles.bold}>Stats</Text>
+            <Text>Tähän vois laittaa montako hyppyä tehny tms pientä tietoa</Text>
+            {/*STATS button, ei tapahu vielä mitään tästä*/}
+            <StyledButton title="View Stats" onPress={() => {}} />
+          </View>
+      
+
+          <View style={{ marginTop: "auto" }}>
+            <StyledButton title="Sign out" onPress={signOut} />
+          </View>
         </>
       ) : (
         <Text style={{ color: theme.colors.textSecondary }}>Profile information not available</Text>
       )}
-      <StyledButton onPress={signOut} title="Sign Out" />
     </View>
   );
 };
@@ -80,11 +113,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    alignItems: "center",
-    justifyContent: "center",
   },
-  welcomeText: {
+  bold: {
+    fontSize: 22,
+    fontWeight: "600",
+  },
+  headingCard: {
+    fontSize: 24,
+    fontWeight: "600",
+    paddingVertical: 10,
+    paddingTop: 20,
+  },
+    welcomeText: {
     fontSize: 18,
     marginBottom: 20,
+  },
+    avatar: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 12,
+  },
+    nameCard: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+    nameText: {
+    alignItems: "flex-start",
   },
 });
